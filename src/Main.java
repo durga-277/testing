61b9cd7af37c15c0a372a1f091d53c33a26b45a6
durga-277 <152302913+durga-277@users.noreply.github.com>
2023-11-29 18:46:28 +0530

    print statement added

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-<<<<<<< HEAD
         int a=10;
         int b=5;
         int ans =a+b;
-=======
-  // adding the sub file
-  // this time it works or not 
-//>>>>>>> d5b666c8674ee55ef26e03a8ed530900ec6d7d1b
+        System.out.println(ans);
+
 //
 }
 }
