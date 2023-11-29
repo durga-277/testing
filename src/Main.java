34b900806d714922c181e3788506650681a4b023
durga-277 <152302913+durga-277@users.noreply.github.com>
2023-11-29 19:03:11 +0530

    sub method added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
         System.out.println("Hello world!");
         int a=10;
         int b=5;
-        int ans =a+b;
-        System.out.println(ans);
+        int sum =a+b;
+        System.out.println(sum);
+        int i=8,j=3,sub;
+        sub=i-j;
+        System.out.println(sub);
     }
 }
